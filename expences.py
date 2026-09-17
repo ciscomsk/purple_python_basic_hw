9a6b3d82cc6751847ccb2a8274bf212f0f80e032
ciscomsk <kuznetzoff@gmail.com>
2026-09-18 00:21:08 +0300

    hw 2

--- expences.py
+++ expences.py
@@ -1,6 +1,7 @@
 food = int(input("Enter food expenses: "))
 transport = int(input("Enter transport expenses: "))
 entertainment = int(input("Enter entertainment expenses: "))
-sum = food + transport + entertainment
+# sum - встроенная функция
+total = food + transport + entertainment
 avg = sum / 3
 print(sum, avg)
