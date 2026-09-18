7d717a9c51ddb37929d321bbe0c24824a3c6d2ef
ciscomsk <kuznetzoff@gmail.com>
2026-09-18 12:25:34 +0300

    hw 4

---
+++ list_expenses.py
@@ -0,0 +1,7 @@
+expenses = [10, 20, 30, 40, 50, 60, 70]
+total = sum(expenses)
+avg = total / len(expenses)
+minimum = min(expenses)
+maximum = max(expenses)
+res = (total, avg, minimum, maximum)
+print(res)
