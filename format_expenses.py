273483957eda13a443419540b9faeb298db9c35e
ciscomsk <kuznetzoff@gmail.com>
2026-09-19 17:56:28 +0300

    hw 5

--- format_expenses.py
+++ format_expenses.py
@@ -1,13 +1,24 @@
-text = input("Enter text: ").strip().lower()
-
-if text.count("руб") != 1:
+sum_text = input("Enter text: ").strip().lower()
+if sum_text.count("руб") != 1:
     print("Некорректный формат суммы")
     exit()
 
-text_parts = text.split("руб")
-rub_part = text_parts[0].strip()
-cop_part = text_parts[1].strip()
-has_cop = text_parts[1].count("коп") == 1
-total = float(rub_part) if not has_cop else float(f"{rub_part}.{cop_part.split("коп")[0].strip()}")
+sum_parts = sum_text.split("руб")
+rub = sum_parts[0].strip()
+if len(rub) == 0:
+    print("Некорректный формат суммы")
+    exit()
+
+cop_part = sum_parts[1].strip()
+if len(cop_part) != 0 and cop_part.count("коп") != 1:
+    print("Некорректный формат суммы")
+    exit()
+
+cop = cop_part.split("коп")[0].strip()
+if len(cop) > 2:
+    print("Некорректный формат суммы")
+    exit()
+
+total = float(f"{rub}.{cop}")  # если cop_str = '' => float(xx.) - ок
 formatted_total = f"{total:.2f} ₽"
 print(formatted_total)
