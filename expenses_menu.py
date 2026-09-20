41c97bdb87bae78321a8fc27e30884d1cfd92c3c
ciscomsk <kuznetzoff@gmail.com>
2026-09-20 14:37:33 +0300

    hw 6

---
+++ expenses_menu.py
@@ -0,0 +1,20 @@
+menu_items = [
+    "Добавить расход",
+    "Показать все расходы",
+    "Показать сумму и средний расход",
+    "Удалить расход по номеру",
+    "Выход"
+]
+
+while True:
+    for item in menu_items:
+        print(item)
+
+    user_choice = ""
+    while user_choice not in menu_items:
+        user_choice = input("Enter your choice: ")
+        if user_choice not in menu_items:
+            print("Incorrect choice")
+
+    if user_choice == "Выход":
+        break
